045d60963deeb1068046ee1244f200b81260ba18
Clemens Wolff <clewolff@microsoft.com>
2020-03-04 05:51:17 -0500

    Remove create-react-app boilerplate

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { faCheck } from '@fortawesome/free-solid-svg-icons/faCheck';
 import { faCompass } from '@fortawesome/free-solid-svg-icons/faCompass';
 import { faHandshake } from '@fortawesome/free-solid-svg-icons/faHandshake';
@@ -11,6 +8,9 @@ import { faTrophy } from '@fortawesome/free-solid-svg-icons/faTrophy';
 import { faUserAstronaut } from '@fortawesome/free-solid-svg-icons/faUserAstronaut';
 import { faUserNinja } from '@fortawesome/free-solid-svg-icons/faUserNinja';
 import { library } from '@fortawesome/fontawesome-svg-core';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import './index.css';
 
 library.add(
   faCheck,
@@ -24,7 +24,4 @@ library.add(
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
